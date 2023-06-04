f5d631d48eb02c88c9380350378649383ba73d1f
Pedro Willian <70727334+pedrowil12@users.noreply.github.com>
2023-06-04 13:22:49 -0300

    Implementation of logs

--- moneymind/src/main/java/com/fatecapi/moneymind/controller/InstituicoesController.java
+++ moneymind/src/main/java/com/fatecapi/moneymind/controller/InstituicoesController.java
@@ -3,6 +3,8 @@ package com.fatecapi.moneymind.controller;
 
 import com.fatecapi.moneymind.entity.Instituicoes;
 import com.fatecapi.moneymind.service.IInstituicoesService;
+import com.fatecapi.moneymind.service.LogService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +21,18 @@ public class InstituicoesController {
 
     @Autowired
     private IInstituicoesService service;
+    @Autowired
+    private LogService logService;
 
     @GetMapping
     public List<Instituicoes> buscarTodasInstituicoes(){
+    	logService.log("GET", "/instituicoes", "Busca por todas as instituições");
         return service.buscarTodasInstituicoes();
     }
     
     @GetMapping("/buscarPorCnpj")
     public List<Instituicoes> buscarPorCnpj(@RequestParam("cnpj") Integer cnpj) {
+    	logService.log("GET", "/instituicoes/buscarPorCnpj?cnpj=" + cnpj, "Busca por CNPJ: " + cnpj);
     	return service.buscarPorCnpj(cnpj);
     }
 }
--- moneymind/src/main/java/com/fatecapi/moneymind/controller/TarifasPorValoresController.java
+++ moneymind/src/main/java/com/fatecapi/moneymind/controller/TarifasPorValoresController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatecapi.moneymind.entity.TarifasPorValores;
+import com.fatecapi.moneymind.service.LogService;
 import com.fatecapi.moneymind.service.TarifasPorValoresService;
 
 @RestController
@@ -20,17 +21,23 @@ public class TarifasPorValoresController {
 	@Autowired
 	private TarifasPorValoresService service;
 
+    @Autowired
+    private LogService logService;
+	
 	@GetMapping
 	public List<TarifasPorValores> buscarTodasTarifas() {
+    	logService.log("GET", "/tarifasValores", "Busca por todas tarifas");
 		return service.buscarTodasTarifas();
 	}
 	
 	@GetMapping("/bancosMaioresTarifas")
 	public List<TarifasPorValores> buscarMaioresTarifas(@RequestParam("grupo") String grupo, @RequestParam("servico") String servico){
+    	logService.log("GET", "/tarifasValores/bancosMaioresTarifas", "Busca por bancos maiores tarifas");
 		return service.buscarMaioresTarifas(grupo, servico);
 	}
 	@GetMapping("/bancosMenoresTarifas")
 	public List<TarifasPorValores> buscarMenoresTarifas(@RequestParam("grupo") String grupo, @RequestParam("servico") String servico){
+    	logService.log("GET", "/tarifasValores/bancosMenoresTarifas", "Busca por bancos menores tarifas");
 		return service.buscarMenoresTarifas(grupo, servico);
 	}
 	
